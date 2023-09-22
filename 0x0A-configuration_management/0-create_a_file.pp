ed8cb5ebb2e7f9bfdecc16369f624ab087e65135
ZouhairSpace <jeddi.zouhair@gmail.com>
2023-09-22 14:26:53 -0700

    updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# creates a file in /tmp
+# create a file in /tmp with specific requirements
 
-file { '/tmp/school':
-  content =>'I love Puppet',
+file { 'school':
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet',
 }
